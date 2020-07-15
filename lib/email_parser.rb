a7ae2a604c5d26649fbbdeb99ea19bee4caf6794
Kiri Um <kirirotha@gmail.com>
2020-07-15 04:09:45 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,7 +13,7 @@ class EmailAddressParser
   
   def parse
     if @email_addresses.include?(",")
-      @email_addresses.delete!(" ")
+      #@email_addresses.delete!(" ")
       @parsed_email =  @email_addresses.split(",")
     else
       @parsed_email = @email_addresses.split(" ")
