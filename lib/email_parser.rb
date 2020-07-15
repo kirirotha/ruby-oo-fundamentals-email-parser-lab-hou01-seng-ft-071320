fe465f4a8474d4ec780030c33184275f2eb8d55b
Kiri Um <kirirotha@gmail.com>
2020-07-15 04:04:14 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,8 +6,8 @@
 
 class EmailAddressParser
   attr_accessor :email_addresses
-  def initialize(name)
-    @name = name
+  def initialize(email_addresses)
+    @email_addresses = email_addresses
   end
   
   
